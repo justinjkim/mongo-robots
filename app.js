e934b9a7ded55be5829d9e99bc4a24b344a04e0c
Justin Kim <jjkim928@gmail.com>
2017-08-28 14:43:38 -0400

    separated robots by job status

--- app.js
+++ app.js
@@ -13,11 +13,29 @@ app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 
 
-app.use('/', function (req, res) {
+app.get('/', function (req, res) {
   MongoClient.connect(mongoURL, function (err, db) {
     const robots = db.collection('robots');
     robots.find({}).toArray(function (err, docs) {
       res.render("index", {robots: docs})
+    })
+  })
+});
+
+app.get('/unemployed', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": null}).toArray(function (err, docs) {
+      res.render("unemployed", {robots: docs})
+    })
+  })
+});
+
+app.get('/employed', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": {$nin: [null]}}).toArray(function (err, docs) {
+      res.render("unemployed", {robots: docs})
     })
   })
 })
