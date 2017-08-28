e4d28bb5d3c8b75fc01a435af44ade39e7bc2f60
Justin Kim <jjkim928@gmail.com>
2017-08-28 13:58:18 -0400

    added a listening port

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const mongoURL = 'mongodb://localhost:27017/newdb';
+
+const app = express();
+
 
 app.use('/', function (req, res) {
   MongoClient.connect(mongoURL, function (err, db) {
@@ -10,3 +16,8 @@ app.use('/', function (req, res) {
     })
   })
 })
+
+
+app.listen(3000, function (req, res) {
+	console.log('Starting up Express app...');
+});
